03840d2c71cf1a9c146006d66cd1c8376e87c705
Evatitan <yinguihua2014@gmail.com>
2022-09-16 09:49:22 +0200

    clear code

--- todolist/src/components/Item/index.jsx
+++ todolist/src/components/Item/index.jsx
@@ -1,49 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import "./index.css"
+import './index.css';
 
 export default class Item extends React.Component {
+	state = { mouse: false };
 
-    state={mouse:false}
+	static propTypes = {
+		updateTodo: PropTypes.func.isRequired,
+		deleteTodo: PropTypes.func.isRequired,
+		id: PropTypes.string.isRequired,
+		name: PropTypes.string.isRequired,
+		done: PropTypes.bool.isRequired
+	};
 
-    static propTypes={
-        updateTodo:PropTypes.func.isRequired,
-        deleteTodo:PropTypes.func.isRequired,
-        id:PropTypes.string.isRequired,
-        name:PropTypes.string.isRequired,
-        done:PropTypes.bool.isRequired
-    }
+	handleMouse = (flag) => {
+		return () => {
+			this.setState({ mouse: flag });
+		};
+	};
 
-    handleMouse=(flag)=>{
-        return ()=>{
-            //console.log(flag);
-            this.setState({mouse:flag})
-        }
-    }
+	handleUpdate = (id) => {
+		return (event) => {
+			this.props.updateTodo(id, event.target.checked);
+		};
+	};
 
-    handleUpdate=(id)=>{
-        return(event)=>{
-            console.log(event.target.checked)
-            this.props.updateTodo(id,event.target.checked)
-        }
-    }
+	handleDelete = (id) => {
+		this.props.deleteTodo(id);
+	};
 
-    handleDelete=(id)=>{
-        console.log(id)
-        this.props.deleteTodo(id)
-    }
-
-    render() {
-        const {mouse}= this.state
-        const {id, name, done}= this.props
-        return (
-                <li className="todo_item" onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)} >
-                    <label className="todo_label">
-                        <input type="checkbox" checked={done} onChange={this.handleUpdate(id)}/>
-                        <span className="todo_name">{name}</span>
-                    </label>
-                    <button onClick={()=>this.handleDelete(id)} className="todo_btn" style={{display: mouse? "block" : "none"}}>Delete</button>
-                </li>
-        )
-    }
+	render() {
+		const { mouse } = this.state;
+		const { id, name, done } = this.props;
+		return (
+			<li className="todo_item" onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)}>
+				<label className="todo_label">
+					<input type="checkbox" checked={done} onChange={this.handleUpdate(id)} />
+					<span className="todo_name">{name}</span>
+				</label>
+				<button
+					onClick={() => this.handleDelete(id)}
+					className="todo_btn"
+					style={{ display: mouse ? 'block' : 'none' }}
+				>
+					Delete
+				</button>
+			</li>
+		);
+	}
 }
